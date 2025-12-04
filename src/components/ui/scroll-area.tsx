4e17bd80a3613eb8bd76f12f09561d029b613d8f
ouhammmourachid <rachidouhammou21@gmail.com>
2025-12-04 14:58:52 +0100

    feat: Add ScrollArea UI component and filter-related CSS styles.

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -30,17 +30,19 @@ const ScrollBar = React.forwardRef<
   <ScrollAreaPrimitive.ScrollAreaScrollbar
     ref={ref}
     orientation={orientation}
+    forceMount
     className={cn(
-      "flex touch-none select-none transition-colors",
+      "flex touch-none select-none transition-colors relative",
+      "before:content-[''] before:absolute before:top-0 before:bottom-0 before:left-1/2 before:-translate-x-1/2 before:w-[1px] before:bg-white/15",
       orientation === "vertical" &&
-        "h-full w-2.5 border-l border-l-transparent p-[1px]",
+      "h-full w-2 border-l border-l-transparent p-[2px]",
       orientation === "horizontal" &&
-        "h-2.5 flex-col border-t border-t-transparent p-[1px]",
+      "h-2.5 flex-col border-t border-t-transparent p-[1px]",
       className
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-border" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-white/40" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
