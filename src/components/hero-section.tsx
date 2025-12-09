2a6dda1274cf6a60a7176349d863a3d33f94d82f
ouhammmourachid <rachidouhammou21@gmail.com>
2025-12-09 11:50:50 +0100

    feat: add HeroSection component with animated title and shop button.

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,44 +1,47 @@
-import {Button} from '@/components/ui/button';
+import { Button } from '@/components/ui/button';
 import { ArrowUpRight } from 'lucide-react';
 import styles from '@/styles/main.module.css';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 import { useTranslation } from '@/lib/i18n-utils';
+import Link from 'next/link';
 
 export default function HeroSection() {
-    const { t } = useTranslation();
-    
-    return (
-        <div className="relative h-[900px] overflow-hidden">
-        {/* Background Image */}
-        <div className="absolute inset-0">
-          <img
-            src="banner-2.jpg"
-            alt="Hero Background"
-            className="w-full h-full object-cover"
-          />
-        </div>
+  const { t } = useTranslation();
 
-        {/* Content */}
-        <div className={styles.hero_section_content}>
-          <div className="text-center space-y-8">
-            <motion.div
-              initial={{ y: 100, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              transition={{ duration: 0.5 }}>
-              <h1 className="text-6xl font-bold">
-                {t('hero_title')}
-              </h1>
-            </motion.div>
-            <motion.div
-              initial={{ y: 100, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              transition={{ duration: 0.5, delay: 0.2 }}>
+  return (
+    <div className="relative h-[900px] overflow-hidden">
+      {/* Background Image */}
+      <div className="absolute inset-0">
+        <img
+          src="banner-2.jpg"
+          alt="Hero Background"
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      {/* Content */}
+      <div className={styles.hero_section_content}>
+        <div className="text-center space-y-8">
+          <motion.div
+            initial={{ y: 100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5 }}>
+            <h1 className="text-6xl font-bold">
+              {t('hero_title')}
+            </h1>
+          </motion.div>
+          <motion.div
+            initial={{ y: 100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.2 }}>
+            <Link href="/products">
               <Button className={styles.hero_section_button}>
                 {t('hero_shop_now')} <ArrowUpRight className="w-6 h-6" />
               </Button>
-            </motion.div>
-          </div>
+            </Link>
+          </motion.div>
         </div>
       </div>
-    )
+    </div>
+  )
 }
