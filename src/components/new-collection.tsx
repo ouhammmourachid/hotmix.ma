24d468f15da094a5b9329d089fc85d5f2ff70c30
ouhammmourachid <rachidouhammou21@gmail.com>
2026-07-25 12:19:19 +0100

    feat: implement main landing page, category and new collection components, and multi-language support (AR, EN, FR)

--- src/components/new-collection.tsx
+++ src/components/new-collection.tsx
@@ -31,7 +31,7 @@ export default function NewCollection() {
   }, []);
 
   return (
-    <section className="py-16">
+    <section className="md:py-8">
       <div className="container mx-auto px-4">
         {/* Section Header */}
         <motion.div
